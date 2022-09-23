e7739995322591b927b33ad0e58b47f117bd0ab1
souravrajput40 <sourav.rajput.grkinfo@gmail.com>
2022-09-23 18:05:35 +0530

    changes fixed

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  resources :softwares
-  resources :social_handles
-  resources :skills
-  resources :projects
-  resources :contacts
+  resources :softwares, only: [:index, :show]
+  resources :social_handles, only: [:index, :show]
+  resources :skills, only: [:index, :show]
+  resources :projects, only: [:index, :show]
+  resources :contacts, only: [:index]
+  post 'contacts/send_message'
   get 'home/about'
   root to: 'home#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
